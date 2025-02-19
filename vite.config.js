8b9a52fc6a211b01a767890dfb5f07388b5a56f5
Arnold Fernando <junfernando727@gmail.com>
2025-02-19 09:03:39 +0800

    Update dependencies in package.json and modify Vite configuration for SCSS input

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ['resources/scss/main.scss'],
             refresh: true,
         }),
     ],
